70fe53f837f94988dd82323de9b1bc00a16cb183
Atlast19 <ahleandro18@gmail.com>
2023-11-13 11:10:02 -0300

    carpeta

---
+++ Practic/Practic/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string nombre;
+string nombre2 = "Leandro";
+Console.WriteLine("Cual es tu nombre");
+nombre = Console.ReadLine();
+
+if (nombre == nombre2)
+{
+
+    Console.WriteLine("Pendejo el que lo lea");
+}
+else
+{
+    Console.WriteLine("pon el nombre bien mal pario");
+
+
+}
+
+
+int i = 0;
+do
+{
+    Console.WriteLine(i);
+    i++;
+}
+while (i <= 5);
+
